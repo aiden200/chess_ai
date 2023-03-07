2aed5947ab472f6912ae9cb2ef5d1a7e084c031c
Aiden Chang <62921661+aiden200@users.noreply.github.com>
2023-03-07 12:27:39 -0600

    finished alpha beta pruning

--- chess_engine/chess_game.py
+++ chess_engine/chess_game.py
@@ -4,28 +4,109 @@ from piece_values import *
 
 
 class Chess_game:
-    def __init__(self) -> None:
+    def __init__(self, plies) -> None:
         self.board = chess.Board()
         self.winner = None
-        self.turn = 1
-    def start_game(self):
+        self.turn = 1 # white, min player
+        self.plies = plies #depth of looking, deeper the better but slower
 
+
+    def play_game(self):
+
+        while True:
+            move = self.make_move()
+            if self.winner != None:
+                break
+            self.board.push(move)
+            if self.turn:
+                self.turn = 0
+            else:
+                self.turn = 1
         
         return self.winner
+        
+
 
     def make_move(self):
+        #check win first
+        if self.board.is_checkmate():
+            if self.turn:
+                self.winner = "White"
+            else:
+                self.winner = "Black"
+            return
+        if self.board.is_stalemate():
+            self.winner = "Stalemate"
+            return
+        if self.board.is_insufficient_material():
+            self.winner = "Stalemate"
+            return 
         try:
-            
+            #Reading from openers
             move = chess.polyglot.MemoryMappedReader("./books/human.bin").weighted_choice(self.board).move
             return move
         except:
-            
-            pass
+            #minimax alpha beta
+            if self.turn == 1:
+                move = self.min_value(self.board, self.plies, float('-inf'),float('inf'))[1]
+            else:
+                move = self.max_value(self.board, self.plies, float('-inf'),float('inf'))[1]
+            return move
     
+    def min_value(self, plies : int, alpha : int, beta : int):
+        if plies == 0:
+            return self.evaluate_board(), None
+        best : int = float('inf')
+        final_move = None
+        # get all legal moves and search to find best one for min player
+        legal_moves = self.board.get_legal_moves()
+        for move in legal_moves:
+            self.board.push(move)
+            value = self.max_value(plies-1, alpha, beta)[0]
+            self.board.pop()
+            if value < best:
+                best = value
+                beta = min(beta, value)
+                final_move = move
+            if value <= alpha:
+                return value, final_move
+        return best, final_move
+    
+    
+    def max_value(self, plies : int, alpha : int, beta : int):
+        if plies == 0:
+            return self.evaluate_board(), None
+        best : int = float('-inf')
+        final_move = None
+        # get all legal moves and search to find best one for max player
+        legal_moves = self.board.get_legal_moves()
+        for move in legal_moves:
+            self.board.push(move)
+            value = self.min_value(plies-1, alpha, beta)[0]
+            self.board.pop()
+            if value > best:
+                best = value
+                alpha = max(alpha, value)
+                final_move = move
+            if value >= beta:
+                return value, final_move
+        return best, final_move
+    
+
+
     def evaluate_board(self):
         '''
         White wants to have a lower score
         '''
+        if self.board.is_checkmate():
+            if self.turn:
+                return -9999
+            else:
+                return 9999
+        if self.board.is_stalemate():
+                return 0
+        if self.board.is_insufficient_material():
+                return 0
         counts = {'num_wp':len(self.board.pieces(chess.PAWN, chess.WHITE)),
         'num_bp':len(self.board.pieces(chess.PAWN, chess.BLACK)),
         'num_wn':len(self.board.pieces(chess.KNIGHT, chess.WHITE)),
@@ -46,18 +127,18 @@ class Chess_game:
             piece_score += individual_piece_values[key[-1]]*counts[key]*multiplier
         
         pst_score = 0
-        pst_score += -sum([pawntable[i] for i in self.board.pieces(chess.PAWN, chess.WHITE)]) + sum([-pawntable[chess.square_mirror(i)]for i in self.board.pieces(chess.PAWN, chess.BLACK)])
+        pst_score += -sum([pawntable[i] for i in self.board.pieces(chess.PAWN, chess.WHITE)]) + sum([pawntable[chess.square_mirror(i)]for i in self.board.pieces(chess.PAWN, chess.BLACK)])
 
-        pst_score += -sum([knightstable[i] for i in self.board.pieces(chess.KNIGHT, chess.WHITE)])+ sum([-knightstable[chess.square_mirror(i)]
+        pst_score += -sum([knightstable[i] for i in self.board.pieces(chess.KNIGHT, chess.WHITE)])+ sum([knightstable[chess.square_mirror(i)]
                                 for i in self.board.pieces(chess.KNIGHT, chess.BLACK)])
-        pst_score += -sum([bishopstable[i] for i in self.board.pieces(chess.BISHOP, chess.WHITE)])+ sum([-bishopstable[chess.square_mirror(i)]
+        pst_score += -sum([bishopstable[i] for i in self.board.pieces(chess.BISHOP, chess.WHITE)])+ sum([bishopstable[chess.square_mirror(i)]
                                 for i in self.board.pieces(chess.BISHOP, chess.BLACK)])
-        pst_score += -sum([rookstable[i] for i in self.board.pieces(chess.ROOK, chess.WHITE)]) + sum([-rookstable[chess.square_mirror(i)]
+        pst_score += -sum([rookstable[i] for i in self.board.pieces(chess.ROOK, chess.WHITE)]) + sum([rookstable[chess.square_mirror(i)]
                             for i in self.board.pieces(chess.ROOK, chess.BLACK)])
-        pst_score += -sum([queenstable[i] for i in self.board.pieces(chess.QUEEN, chess.WHITE)]) + sum([-queenstable[chess.square_mirror(i)]
+        pst_score += -sum([queenstable[i] for i in self.board.pieces(chess.QUEEN, chess.WHITE)]) + sum([queenstable[chess.square_mirror(i)]
                                 for i in self.board.pieces(chess.QUEEN, chess.BLACK)])
         #Future implementation the second kings square
-        pst_score += -sum([kingstable[i] for i in self.board.pieces(chess.KING, chess.WHITE)]) + sum([-kingstable[chess.square_mirror(i)]
+        pst_score += -sum([kingstable[i] for i in self.board.pieces(chess.KING, chess.WHITE)]) + sum([kingstable[chess.square_mirror(i)]
                             for i in self.board.pieces(chess.KING, chess.BLACK)])
         
         return piece_score + pst_score
